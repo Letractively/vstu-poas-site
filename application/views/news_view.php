3e7b43d1af83f9fe33a62ffb302d7790c3c1d9f7
????????? <?????????@?????????-??>
2011-11-02 22:14:00 +0300

    ??????? ??????? ??? ??????? ?? ????? ???????

--- application/views/news_view.php
+++ application/views/news_view.php
@@ -1,5 +1,5 @@
 <?php // Полный вывод одной новости
 	echo '<h3>'.$news->name.'</h3>';
-	echo '<p style="float:right">'.$news->time.'</p>'.br();
 	echo '<p>'.$news->text.'</p>';
+	echo br().'<p style="float:right">'.$news->time.'</p>'.br();
 	
