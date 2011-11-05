57aa78aa8772dec68de865cf4f6ce9c89942a938
Arkanif <Arkanif@localhost>
2011-11-05 03:19:49 +0300

    ????????? ???????? ??????? ??? ???????????. ???? ?? ??? ?????? ????.

---
+++ application/models/direction_model.php
@@ -0,0 +1,4 @@
+<?php
+class Direction_model extends CI_Model {
+
+}
---
+++ application/views/admin/directions_view.php
@@ -0,0 +1,5 @@
+<?php
+echo anchor('/admin/directions/add', 'Добавить направление');
+echo br(2);
+/* End of file directions_view.php */
+/* Location: ./application/views/admin/directions_view.php */
