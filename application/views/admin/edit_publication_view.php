bbad7a2d2f951c0bc9bbddb73e9e55d548132b5f
Arkanif <Arkanif@localhost>
2011-11-06 16:25:19 +0300

    ??????????? ????? ?????????????? ??????????

--- application/views/admin/edit_publication_view.php
+++ application/views/admin/edit_publication_view.php
@@ -1,7 +1,77 @@
+<div id="form_edit">
 <?php
+if( !isset($publication->id) )
+{
+	$action = 'add';
+	$button_title = 'Создать публикацию';
+}
+else 
+{
+	$action = 'edit';
+	$button_title = 'Внести изменения в публикацию';
+}
+if (!isset($publication->name_ru)) 			{$publication->name_ru = '';};
+if (!isset($publication->name_en)) 			{$publication->name_en = '';};
+if (!isset($publication->fulltext_ru))      {$publication->fulltext_ru = '';};
+if (!isset($publication->fulltext_en))      {$publication->fulltext_en = '';};
+if (!isset($publication->abstract_ru))      {$publication->abstract_ru = '';};
+if (!isset($publication->abstract_en))      {$publication->abstract_en = '';};
+if (!isset($publication->info_ru))          {$publication->info_ru = '';};
+if (!isset($publication->info_en))          {$publication->info_en = '';};
+if (!isset($publication->year)) 			{$publication->year = '';};
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+
+
+
+
+echo form_open('admin/publications/'.$action.'/action');
+
+echo form_label('Название публикации (русское)', 'publication_name_ru', array('class'=>'inline-block'));
+echo form_textarea('publication_name_ru', $publication->name_ru, 'maxlength="150" style = width:400px');
+echo br(2);
+
+echo form_label('Год публикации', 'publication_year', array('class'=>'inline-block'));
+echo form_input('publication_year', $publication->year, 'maxlength="4" style = width:50px');
+echo br(2);
+
+echo form_label('Ссылка, по которой можно найти текст публикации (на русском)', 'publication_fulltext_ru', array('class'=>'inline-block'));
+echo form_input('publication_fulltext_ru', $publication->fulltext_ru, 'maxlength="300" style = width:400px');
+echo br(2);
+
+echo form_label('Ссылка, по которой можно найти аннотацию публикации (на русском)', 'publication_abstract_ru', array('class'=>'inline-block'));
+echo form_input('publication_abstract_ru', $publication->abstract_ru, 'maxlength="300" style = width:400px');
+echo br(2);
+
+echo form_label('Дополнительная информация (на русском)', 'publication_info_ru', array('class'=>'inline-block'));
+echo form_textarea('publication_info_ru', $publication->info_ru, 'maxlength="150" style = width:400px');
+echo br(2);
+
+echo '<hr>';
+
+echo form_label('Название публикации (английское)', 'publication_name_en', array('class'=>'inline-block'));
+echo form_textarea('publication_name_en', $publication->name_en, 'maxlength="150" style = width:400px');
+echo br(2);
+
+echo form_label('Ссылка, по которой можно найти текст публикации (на английском)', 'publication_fulltext_en', array('class'=>'inline-block'));
+echo form_input('publication_fulltext_en', $publication->fulltext_en, 'maxlength="300" style = width:400px');
+echo br(2);
+
+echo form_label('Ссылка, по которой можно найти аннотацию публикации (на английском)', 'publication_abstract_en', array('class'=>'inline-block'));
+echo form_input('publication_abstract_en', $publication->abstract_en, 'maxlength="300" style = width:400px');
+echo br(2);
+
+echo form_label('Дополнительная информация (на английском)', 'publication_info_en', array('class'=>'inline-block'));
+echo form_textarea('publication_info_en', $publication->info_en, 'maxlength="150" style = width:400px');
+echo br(2);
+
+if( isset($publication->id) )
+{
+	echo form_hidden('publication_id', $publication->id);
+}
+echo br(2);
+echo form_submit('user_submit', $button_title);
+echo form_close();
 ?>
+</div>
