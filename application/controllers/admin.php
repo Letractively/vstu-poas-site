86ad71b4a57d55685eff226b9e2418ffb7ef391d
Arkanif <Arkanif@localhost>
2011-11-08 15:13:55 +0300

    ?????????? ?????? ? ??????????????? ???????, ????? ??-?? ?????? ???????????? ?????????? ????? ????????? ????????. ??? ????, ???? ?? ???? ?????????? ???????, ???????? ??????.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -303,6 +303,7 @@ class Admin extends CI_Controller {
 	function logout()
 	{
 		$this->session->sess_destroy();
+        redirect($this->config->item('base_url'));
 	}
 }
 
--- application/views/templates/admin_view.php
+++ application/views/templates/admin_view.php
@@ -46,7 +46,7 @@
 			<li><a href="/admin/directions">Направления</a></li>
             <li><a href="/admin/publications">Публикации</a></li>
             <li><a href="/admin/partners">Партнеры</a></li>
-			<li><a href="/user/logout">Выйти</a></li>
+			<li><a href="/admin/logout">Выйти</a></li>
 		</ul>	
 		</div>
 
