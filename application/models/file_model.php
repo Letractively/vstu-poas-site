bf3f918a0b225d7ecce73289dff6bd1f9c443c87
Arkanif <Arkanif@localhost>
2011-11-16 00:03:54 +0400

    ??? ????? ?????? ?????

---
+++ application/models/file_model.php
@@ -0,0 +1,23 @@
+<?php
+class File_model extends CI_Model{
+    function get_file_path($fileid)
+    {
+        $file = $this->db
+                            ->select('name')
+                            ->get_where(TABLE_FILES, array('id' => $fileid))
+                            ->result();
+        return $file ? $file[0]->name : NULL;
+    }
+    function delete_file($fileid)
+    {
+        if ($fileid != NULL)
+        {
+            $oldpath = $this->get_file_path($fileid);
+            if($oldpath)
+            {            
+                unlink($oldpath);
+            }
+            $this->db->delete(TABLE_FILES, array('id' => $fileid));
+        }
+    }
+}
