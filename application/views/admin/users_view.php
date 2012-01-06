295872d0a8071b54cdce0e226587404436c927c5
Arkanif <Arkanif@localhost>
2012-01-06 20:12:56 +0400

    views\admin\users_view.php ???????, ??? ?????????, ???? ? ?????? ?? ?????

--- application/views/admin/users_view.php
+++ application/views/admin/users_view.php
@@ -2,7 +2,7 @@
 $groups = array('Не определено', 'Администратор', 'Студент', 'Преподаватель');
 echo anchor('/admin/users/add', 'Добавить пользователя');
 echo br(2);
-    
+
     $args = '';
     // upload_path
     $args .= "'./uploads/users/',";
@@ -13,17 +13,17 @@ echo br(2);
     // max_width
     $args .= "'800',";
     // max_height
-    $args .= "'600',";    
+    $args .= "'600',";
     //table_name
     $args .= "'" . TABLE_USERS . "',";
     //field_name
     $args .= "'photo',";
-    
-    // +    
+
+    // +
     // record_id
-    // id пользователя добавляется для каждого пользователя    
+    // id пользователя добавляется для каждого пользователя
     // full_url ссылка на файл, если он существует
-    
+
 $data['rows'] = array();
 $data['classes'] = array('users', '', '', '');
 $data['headers'] = array('ФИО','Группа','','');
@@ -44,17 +44,17 @@ foreach($users as $user)
         img( '/images/admin/buttons/user.png'),
             array(	'class' => 'button_edit_file',
                     'title' => 'Изменить фотографию пользователя '.$user->id,
-                    'onclick' => 'fileLoader(' . 
-                                    $args . 
-                                    "'" . 
-                                    $user->id . 
-                                    "','" . 
-                                    $this->config->item('base_url') . 
+                    'onclick' => 'fileLoader(' .
+                                    $args .
+                                    "'" .
+                                    $user->id .
+                                    "','" .
+                                    $this->config->item('base_url') .
                                     $this->{MODEL_USER}->get_photo($user->id) .
                                     "')")
         );
-    
-    $tablerow[] = anchor(   'admin/users/edit/' . $user->id, 
+
+    $tablerow[] = anchor(   'admin/users/edit/' . $user->id,
                                 $user->surname . ' '.
                                 $user->name . ' ' .
                                 $user->patronymic);
