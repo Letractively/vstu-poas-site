d4c3594fc486c154b9e80b8aa6721fd40dad43b9
Arkanif <Arkanif@localhost>
2012-01-07 18:12:20 +0400

    ??????? ajax ?? ???????? ????????? ????? ???????, ??? ?????? ?? ????? ???????????? ????????.

--- application/views/admin/partners_view.php
+++ application/views/admin/partners_view.php
@@ -3,21 +3,21 @@ echo anchor('/admin/partners/add', 'Добавить партнера');
 echo br(2);
 $data['rows'] = array();
 $data['headers'] = array('id', 'Название', 'Изображение', '');
-    $args = '';
-    // upload_path
-    $args .= "'./uploads/partners/',";
-    // allowed_types
-    $args .= "'gif|jpg|png|jpeg',";
-    // max_size
-    $args .= "'1000',";
-    // max_width
-    $args .= "'1024',";
-    // max_height
-    $args .= "'800',";
-    //table_name
-    $args .= "'" . TABLE_PARTNERS . "',";
-    //field_name
-    $args .= "'image',";
+//    $args = '';
+//    // upload_path
+//    $args .= "'./uploads/partners/',";
+//    // allowed_types
+//    $args .= "'gif|jpg|png|jpeg',";
+//    // max_size
+//    $args .= "'1000',";
+//    // max_width
+//    $args .= "'1024',";
+//    // max_height
+//    $args .= "'800',";
+//    //table_name
+//    $args .= "'" . TABLE_PARTNERS . "',";
+//    //field_name
+//    $args .= "'image',";
 
     // +
     // record_id
@@ -31,10 +31,11 @@ foreach($partners as $partner)
             array(	'class' => 'button_delete',
                     'title' => 'Удалить партнера ' . $partner->name)
         );
-//    $button_edit_photo = anchor('#',
-//        img( '/images/admin/buttons/user.png'),
-//            array(	'class' => 'button_edit_file',
-//                    'title' => 'Изменить изображение для партнера ' . $partner->id,
+    $button_edit_photo = anchor('#',
+        img( '/images/admin/buttons/user.png'),
+            array(	'class' => 'button_edit_file',
+                    'title' => 'Изменить изображение для партнера ' . $partner->id,
+                    'onclick' => "advFileLoader('partner', '$partner->id')")
 //                    'onclick' => 'fileLoader(' .
 //                                    $args .
 //                                    "'" .
@@ -43,8 +44,7 @@ foreach($partners as $partner)
 //                                    $this->config->item('base_url') .
 //                                    $this->{MODEL_PARTNER}->get_image($partner->id) .
 //                                    "')")
-//        );
-    $button_edit_photo = 'редактировать изображение';
+        );
     $tablerow[] = $partner->id;
     $tablerow[] = anchor('admin/partners/edit/' . $partner->id,$partner->name);
     $tablerow[] = $button_edit_photo;
