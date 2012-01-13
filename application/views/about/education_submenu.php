9ee8b9cbe0cbd28fc4ebad9f1065ade64b3060b0
Arkanif <Arkanif@localhost>
2012-01-13 08:48:24 +0400

    ?????????? ?????? ? ???? ??????? ? ??????? ????????????

--- application/views/about/education_submenu.php
+++ application/views/about/education_submenu.php
@@ -1,17 +1,19 @@
 <div class="education-submenu">
-    <?php
-        lang();
-        
-        $section == 'index' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_education_general', '/about/education/index', 'class="button"');
-        
-        $section == 'bachelor' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_education_bachelor', '/about/education/bachelor', 'class="button"');
-        
-        $section == 'magistracy' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_education_magistracy', '/about/education/magistracy', 'class="button"');
-        
-        $section == 'pgdoc' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_education_pgdoc', '/about/education/pgdoc', 'class="button"');
-    ?>
+	<div class="top-menu">
+	    <?php
+	        lang();
+	        
+	        $section == 'index' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_education_general', '/about/education/index', 'class="button"');
+	        
+	        $section == 'bachelor' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_education_bachelor', '/about/education/bachelor', 'class="button"');
+	        
+	        $section == 'magistracy' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_education_magistracy', '/about/education/magistracy', 'class="button"');
+	        
+	        $section == 'pgdoc' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_education_pgdoc', '/about/education/pgdoc', 'class="button"');
+	    ?>
+    </div>
 </div>
