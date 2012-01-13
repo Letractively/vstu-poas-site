f3869072c0209a56c3d93bed309965114175fdc3
Arkanif <Arkanif@localhost>
2012-01-13 08:48:24 +0400

    ?????????? ?????? ? ???? ??????? ? ??????? ????????????

--- application/views/about/scientific_submenu.php
+++ application/views/about/scientific_submenu.php
@@ -1,16 +1,18 @@
 <div class="scientific-submenu">
-    <?php
-        lang();
-        $section == 'index' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_scientific_general', '/about/scientific/index', 'class="button"');
-        
-        $section == 'publications' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_scientific_publications', '/about/scientific/publications', 'class="button"');
-        
-        $section == 'projects' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_scientific_projects', '/about/scientific/projects', 'class="button"');
-        
-        $section == 'directions' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
-        $func('page_scientific_directions', '/about/scientific/directions', 'class="button"');        
-    ?>
+	<div class="top-menu">
+	    <?php
+	        lang();
+	        $section == 'index' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_scientific_general', '/about/scientific/index', 'class="button"');
+	        
+	        $section == 'publications' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_scientific_publications', '/about/scientific/publications', 'class="button"');
+	        
+	        $section == 'projects' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_scientific_projects', '/about/scientific/projects', 'class="button"');
+	        
+	        $section == 'directions' ? $func = 'menu_item_wii_off' : $func = 'menu_item_wii';
+	        $func('page_scientific_directions', '/about/scientific/directions', 'class="button"');        
+	    ?>
+    </div>
 </div>
